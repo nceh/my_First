72ff7c6834b4f31f04f4ebd208f3febc995b4f2b
nceh <nceh.mousavinezhad@gmail.com>
2021-08-31 18:05:07 +0430

    nahaei

--- 09.06.00- jadi.py
+++ 09.06.00- jadi.py
@@ -285,39 +285,6 @@ for this_one in list(count.keys()):
 
 
 
-# In[29]:
-
-
-def lo_counter(a):
-    l_counter=0
-    lo=a.lower()
-    for i in range (0,int(len(a))):
-        if a[i]==lo[i]:
-            l_counter+=1
-    return l_counter
-def up_counter(b):
-    u_counter=0
-    up=a.upper()
-    for j in range (0,int(len(a))):
-        if a[j]==up[j]:
-            u_counter+=1
-    return u_counter
-a= input()
-vaziat=True
-
-if lo_counter(a) == up_counter(a):
-    vaziat=True
-if lo_counter(a) > up_counter(a):
-    vaziat=True
-if lo_counter(a) < up_counter(a):
-    vaziat=False
-if vaziat:
-    a=a.lower()
-else:
-    a=a.upper()
-print(a)
-
-
 # In[33]:
 
 
@@ -351,30 +318,6 @@ if vaziat == True:
 else:
     a = a.upper()
 print(a)
-
-
-# In[ ]:
-
-
-def palindrome(a):
-    lenny=len(a)
-    vaziat=True
-    temp=a[::-1]
-    for i in range (0,lenny):
-        if a[i]!=temp[i]:
-            vaziat=False
-            
-    return vaziat
-
-
-a=input()
-a=a.lower()
-
-if palindrome(a):
-    print("palindrome")
-    
-else:
-    print("not palindrome")
 
 
 # In[ ]:
