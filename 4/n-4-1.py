0860a7a631c5b05d9bec0211022a973f5aa9db02
nceh <nceh.mousavinezhad@gmail.com>
2021-09-08 14:59:53 +0430

    sqrt

---
+++ 4/n-4-1.py
@@ -0,0 +1,13 @@
+from math import sqrt
+
+total = int(input())
+
+
+def jazr(a):
+    rishe = sqrt(a)
+    return rishe
+
+
+for i in range(0, total):
+    adad = float((input()))
+    print(jazr(adad))
