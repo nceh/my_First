643aa9efe62e1cad94aef82919a7f36de0fc821b
nceh <nceh.mousavinezhad@gmail.com>
2021-09-08 13:53:41 +0430

    dictionary

---
+++ 3/n-3-12.py
@@ -0,0 +1,13 @@
+from collections import OrderDict
+
+loghatnamee = {}
+a = int(input())
+for i in range(0, a):
+    a, b = input().split(' ')
+    loghatnamee[a] = b
+
+loghatnamee = {"hello": "salam",
+               "goodbye": "khodafez",
+               "say": "goftan",
+               "we": "ma",
+               "you": "shoma"}
