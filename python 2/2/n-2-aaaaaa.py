e302a9377483b2b765dca0c793b9e0dd049b103d
nceh <nceh.mousavinezhad@gmail.com>
2021-09-10 17:26:45 +0430

    fasl 2 --- video 6

---
+++ python 2/2/n-2-aaaaaa.py
@@ -0,0 +1,38 @@
+class Device:
+    count = 0
+
+    def __init__(self, ip, mac, name):
+        self.ip = ip
+        self.mac_address = mac
+        self.name = name
+
+        # result
+
+        if result:
+            self.status = 'active'
+        else:
+            self.status = 'unknown'
+
+        def get_status(self):
+    # return
+
+
+class TV(Device):
+    def turn_on(self):
+        # connect
+        pass
+
+    def turn_off(self):
+
+
+# connect
+
+class Thermo(Device):
+    def get_degree(self):
+        # connect
+        return result
+
+
+class SmartTV(TV):
+    def turn_on(self):
+# connect
